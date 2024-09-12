dc37d563c35c6f5646396ac9c066c5e7a859bfb6
Joshua Smith <joshsmith@nceas.ucsb.edu>
2024-09-12 10:16:53 -0700

    working

--- analyses/RM_analyses.R
+++ analyses/RM_analyses.R
@@ -145,100 +145,17 @@ p <- ggplot(pred_combined, aes(x = x, y = predicted)) +
             hjust = 1.1, vjust = 1.1, size = 3) +  # Add the labels for p-value and coefficient
   base_theme
 
+p
 
-
-ggsave(p, filename = file.path(figdir, "Fig1_glm_model.png"), 
-       width =7, height = 5, units = "in", dpi = 600, bg = "white")
-
-
+#ggsave(p, filename = file.path(figdir, "Fig1_glm_model.png"), 
+ #     width =7, height = 5, units = "in", dpi = 600, bg = "white")
 
 
 ################################################################################
 #step3 - plot Figure 2
 
-
-g1 <- ggplot(recruit_build1, aes(x = density20m2_macro_stipes, y = recruit_density)) +
-  geom_point() +
- # geom_smooth(method = "nls", 
-  #            formula = y ~ a * exp(b * x), 
-   #           method.args = list(start = list(a = 50, b = -1)),  
-    #          se = FALSE) +
-  labs(title = "", 
-       x = "Adult kelp stipe density", 
-       y = "Kelp recruit density")+
-  theme_bw() + base_theme
-g1
-
-
-
-g2 <- ggplot(recruit_build1, aes(x = density20m2_macro_plants, y = recruit_density)) +
-  geom_point() +
-  # geom_smooth(method = "nls", 
-  #            formula = y ~ a * exp(b * x), 
-  #           method.args = list(start = list(a = 50, b = -1)),  
-  #          se = FALSE) +
-  labs(title = "", 
-       x = "Adult kelp plant \ndensity (n. plants per 20m²)", 
-       y = "Kelp recruit density")+
-  theme_bw() + base_theme
-g2
-
-ggpubr::ggarrange(g1, g2)
-
-
-
-
-
-
-
-
-#
-
-
-#rm(list=ls())
-
-#### MUST RUN Step1_process_recovery_data first! ####
-
-################################################################################
-#load packages and set directories
-librarian::shelf(tidyverse, here)
-
-figdir <- here::here("figures")
-
-
-################################################################################
-#step1 - process data
-
-#calculate total recruit density. Recruit - recruit and juvenile
-
-recruit_build1 <- quad_build_combined %>%
-  mutate(
-    # Calculate recruit density
-    recruit_density = lamr + macr + macj + nerj + ptej + 
-      lsetj + eisj,
-    # Calculate gastropod density
-    gast_density = tegula_densitym2 + pomaulax_densitym2,
-    # Calculate urchin & gastropod density
-    grazer_density = gast_density + purple_urchin_densitym2,
-    # Calculate proportion urchins exposed
-    prop_exp = ifelse(purple_urchin_densitym2 > 0, 
-                      (purple_urchin_densitym2 - purple_urchin_conceiledm2) / purple_urchin_densitym2,
-                      ifelse(purple_urchin_densitym2 == 0 & purple_urchin_conceiledm2 == 0, NA, 1)))%>%
-  #select focal vars to reduce size of df
-  select(survey_date, site, site_type, zone, transect, quadrat, recruit_density, substrate, 
-         purple_urchin_densitym2, prop_exp, gast_density, grazer_density,
-         density20m2_macro_plants, density20m2_macro_stipes, substrate,
-         relief, risk) %>%
-  mutate(substrate = factor(substrate, levels = c("Sand","Cobble","Boulder","Bedrock")))
-#drop outliers
-#filter(recruit_density > 1)
-
-################################################################################
-#step2 - plot Figure 1
-
-
-base_theme <-  theme(axis.text=element_text(size=8, color = "black"),
-                     axis.title=element_text(size=9,color = "black"),
+base_theme2 <-  theme(axis.text=element_text(size=9, color = "black"),
+                     axis.title=element_text(size=10,color = "black"),
                      plot.tag=element_text(size=8,color = "black"),
                      plot.title=element_text(size=9,color = "black", face = "bold"),
                      # Gridlines
@@ -251,123 +168,25 @@ base_theme <-  theme(axis.text=element_text(size=8, color = "black"),
                      #legend.key = element_rect(fill = "white"), # Set it to transparent
                      legend.spacing.y = unit(0.4, "cm"),  
                      legend.text=element_text(size=8,color = "black"),
-                     legend.title=element_blank(),
+                     legend.title=element_text(size=8,color = "black"),
                      #legend.key.height = unit(0.1, "cm"),
                      #legend.background = element_rect(fill=alpha('blue', 0)),
                      #facets
                      strip.text = element_text(size=8, face = "bold",color = "black", hjust=0),
                      strip.background = element_blank())
 
+# Single bubble plot with plant density on the x-axis, bubble size scaled to stipe density, forestgreen fill, and black outline
+g <- ggplot(recruit_build1, aes(x = density20m2_macro_plants, y = recruit_density, size = density20m2_macro_stipes)) +
+  geom_jitter(alpha = 0.6, width = 0.3, height = 0.1, color = "black", fill = "forestgreen", shape = 21) +  # Add jitter, black outline, and forestgreen fill
+  labs(title = "",
+       x = "Adult kelp plant density \n(n. plants per 20m²)",
+       y = "Kelp recruit density",
+       size = "Stipes per plant") +  # Updated legend title
+  theme_bw() + base_theme2
+g
 
-p1 <- ggplot(recruit_build1, aes(x = gast_density, y = recruit_density)) +
-  geom_point() +
-  geom_smooth(method = "nls", 
-              formula = y ~ a * exp(b * x), 
-              method.args = list(start = list(a = 50, b = -1)),  
-              se = FALSE) +
-  labs(title = "", 
-       x = "Gastropod density", 
-       y = "Kelp recruit Density")+
-  theme_bw() + base_theme
-p1
-
-
-
-p2 <- ggplot(recruit_build1, aes(x = purple_urchin_densitym2, y = recruit_density)) +
-  geom_point() +
-  geom_smooth(method = "nls", 
-              formula = y ~ a * exp(b * x), 
-              method.args = list(start = list(a = 50, b = -1)),  
-              se = FALSE) +
-  labs(title = "", 
-       x = "Purple sea urchin density", 
-       y = "Kelp recruit Density")+
-  theme_bw() + base_theme
-#p2
-
-p3 <- ggplot(recruit_build1, aes(x = grazer_density, y = recruit_density)) +
-  geom_point() +
-  geom_smooth(method = "nls", 
-              formula = y ~ a * exp(b * x), 
-              method.args = list(start = list(a = 50, b = -1)),  
-              se = FALSE) +
-  labs(title = "", 
-       x = "COmbined grazer density", 
-       y = "Kelp recruit Density")+
-  theme_bw() + base_theme
-#p3
-
-
-
-p4 <- ggplot(recruit_build1, aes(x = prop_exp, y = recruit_density)) +
-  geom_point() +
-  geom_smooth(method = "nls", 
-              formula = y ~ a * exp(b * x), 
-              method.args = list(start = list(a = 50, b = -1)),  
-              se = FALSE) +
-  labs(title = "", 
-       x = "Proportion exposed sea urchins", 
-       y = "Kelp recruit Density")+
-  theme_bw() + base_theme
-#p4
-
-p <- ggpubr::ggarrange(p1,p2,p3,p4)
-p
-
-
-#run GLM
-# Fit the GLM model
-glm_model <- glm(recruit_density ~ gast_density + purple_urchin_densitym2 + prop_exp +
-                   gast_density * purple_urchin_densitym2,
-                 data = recruit_build1, family = "poisson")
-
-summary(glm_model)
-
-
-install.packages("ggeffects")
-library(ggeffects)
-
-# Generate the effects for the GLM model
-effects <- ggeffect(glm_model, terms = c("gast_density", "purple_urchin_densitym2", "prop_exp"))
-
-# Plot the effects using ggplot2
-plot(effects) +
-  labs(title = "Estimated Marginal Means of Recruit Density",
-       x = "Predictor",
-       y = "Estimated Marginal Mean") +
-  theme_minimal()
-
-################################################################################
-#step3 - plot Figure 2
-
-
-g1 <- ggplot(recruit_build1, aes(x = density20m2_macro_stipes, y = recruit_density)) +
-  geom_point() +
-  # geom_smooth(method = "nls", 
-  #            formula = y ~ a * exp(b * x), 
-  #           method.args = list(start = list(a = 50, b = -1)),  
-  #          se = FALSE) +
-  labs(title = "", 
-       x = "Adult kelp stipe density", 
-       y = "Kelp recruit density")+
-  theme_bw() + base_theme
-g1
-
-
-
-g2 <- ggplot(recruit_build1, aes(x = density20m2_macro_plants, y = recruit_density)) +
-  geom_point() +
-  # geom_smooth(method = "nls", 
-  #            formula = y ~ a * exp(b * x), 
-  #           method.args = list(start = list(a = 50, b = -1)),  
-  #          se = FALSE) +
-  labs(title = "", 
-       x = "Adult kelp plant \ndensity (n. plants per 20m²)", 
-       y = "Kelp recruit density")+
-  theme_bw() + base_theme
-g2
-
-ggpubr::ggarrange(g1, g2)
+#ggsave(g, filename = file.path(figdir, "Fig2_bubble_plot.png"), 
+ #     width =6, height = 5, units = "in", dpi = 600, bg = "white")
 
 
 ################################################################################
